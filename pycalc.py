893a86a25326006ab4df6c68a35e0d9e325acd34
Leodanis Pozo Ramos <lpozor78@gmail.com>
2019-06-13 11:07:17 -0400

    Create the GUI

--- pycalc.py
+++ pycalc.py
@@ -42,10 +42,69 @@ class PyCalcUi(QMainWindow):
         """UI initializer."""
         super().__init__()
         self.setWindowTitle("PyCalc")
+        self.setFixedSize(235, 235)
         self.generalLayout = QVBoxLayout()
         self._centralWidget = QWidget(self)
         self.setCentralWidget(self._centralWidget)
         self._centralWidget.setLayout(self.generalLayout)
+        self._createDisplay()
+        self._createButtons()
+
+    def setDisplayText(self, text):
+        """Set display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def displayText(self):
+        """Get display's text."""
+        return self.display.text()
+
+    def clearDisplay(self):
+        """Clear the display."""
+        self.setDisplayText('')
+
+    def _createDisplay(self):
+        """Create display."""
+        self.display = QLineEdit()
+        self.display.setFixedHeight(35)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+
+        self.generalLayout.addWidget(self.display)
+
+    def _createButtons(self):
+        """Create buttons."""
+        self.buttons = {}
+        buttonsLayout = QGridLayout()
+        #     btn_text | position in the QGridLayout
+        buttons = {'7': (0, 0),
+                   '8': (0, 1),
+                   '9': (0, 2),
+                   '/': (0, 3),
+                   'C': (0, 4),
+                   '4': (1, 0),
+                   '5': (1, 1),
+                   '6': (1, 2),
+                   '*': (1, 3),
+                   '(': (1, 4),
+                   '1': (2, 0),
+                   '2': (2, 1),
+                   '3': (2, 2),
+                   '-': (2, 3),
+                   ')': (2, 4),
+                   '0': (3, 0),
+                   '00': (3, 1),
+                   '.': (3, 2),
+                   '+': (3, 3),
+                   '=': (3, 4),
+                   }
+
+        for btn_text, pos in buttons.items():
+            self.buttons[btn_text] = QPushButton(btn_text)
+            self.buttons[btn_text].setFixedSize(40, 40)
+            buttonsLayout.addWidget(self.buttons[btn_text], pos[0], pos[1])
+
+        self.generalLayout.addLayout(buttonsLayout)
 
 
 # Client code
