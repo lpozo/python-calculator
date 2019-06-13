bdbba27f2eaf3af926ff238028f89ac7a0e5dc51
Leodanis Pozo Ramos <lpozor78@gmail.com>
2019-06-13 11:06:13 -0400

    Create the application's skeleton

--- pycalc.py
+++ pycalc.py
@@ -9,3 +9,51 @@
 # SOFTWARE.
 
 """PyCalc is a Python Calculator with PyQt5."""
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QVBoxLayout
+
+
+class PyCalcCtrl:
+    """PyCalc Controller class."""
+
+    def __init__(self):
+        """Controller initializer."""
+        super(PyCalcCtrl, self).__init__()
+        # Application
+        self._pycalc = QApplication(sys.argv)
+        # GUI
+        self._ui = PyCalcUi()
+
+    def runApp(self):
+        """Run the application."""
+        self._ui.show()
+        sys.exit(self._pycalc.exec_())
+
+
+class PyCalcUi(QMainWindow):
+    """PyCalc View or UI class."""
+
+    def __init__(self):
+        """UI initializer."""
+        super().__init__()
+        self.setWindowTitle("PyCalc")
+        self.generalLayout = QVBoxLayout()
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+        self._centralWidget.setLayout(self.generalLayout)
+
+
+# Client code
+def main():
+    """Main function."""
+    app_ctrl = PyCalcCtrl()
+    app_ctrl.runApp()
+
+
+if __name__ == '__main__':
+    main()
