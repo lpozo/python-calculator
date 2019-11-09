2ba2ca1d0e3de1e376e892b63a62f829e007365a
Leodanis Pozo Ramos <lpozor78@gmail.com>
2019-11-09 16:32:36 -0500

    Update comments

--- pycalc.py
+++ pycalc.py
@@ -39,21 +39,26 @@ def pyqt5_app():
     """PyQt5 implementation."""
     PyCalcConfig('config.ini').write()
 
-    # Import QApplication and the required widgets from PyQt5.QtWidgets
+    # Import QApplication from PyQt5.QtWidgets
     from PyQt5.QtWidgets import QApplication
 
+    # Import the view and the controller
     from pycalc.qt_view import PyCalcUi
     from pycalc.qt_controller import PyCalcCtrl
 
     # Create an instance of `QApplication`
     pycalc = QApplication(sys.argv)
+
     # Create the calculator's GUI
     view = PyCalcUi()
+
     # Create the model
     model = PyCalcModel()
+
     # Create the controller and run it
     controller = PyCalcCtrl(model=model, view=view)
     controller.run()
+
     # Execute calculator's main loop
     sys.exit(pycalc.exec_())
 
@@ -62,21 +67,26 @@ def pyside2_app():
     """PySide2 implementation."""
     PyCalcConfig('config.ini').write()
 
-    # Import QApplication and the required widgets from PyQt5.QtWidgets
+    # Import QApplication from PyQt5.QtWidgets
     from PySide2.QtWidgets import QApplication
 
+    # Import the view and the controller
     from pycalc.ps2_view import PyCalcUi
     from pycalc.ps2_controller import PyCalcCtrl
 
-    # Create an instance of `QApplication`
+    # Create an instance of QApplication
     pycalc = QApplication(sys.argv)
+
     # Create the calculator's GUI
     view = PyCalcUi()
+
     # Create the model
     model = PyCalcModel()
+
     # Create the controller and run it
     controller = PyCalcCtrl(model=model, view=view)
     controller.run()
+
     # Execute calculator's main loop
     sys.exit(pycalc.exec_())
 
@@ -85,30 +95,49 @@ def tkinter_app():
     """Tkinter implementation."""
     PyCalcConfig('config.ini').write()
 
+    # Import the view and the controller
     from pycalc.tk_view import PyCalcUi
     from pycalc.tk_controller import PyCalcCtrl
 
+    # Create the calculator's GUI
     view = PyCalcUi()
+
+    # Create the model
     model = PyCalcModel()
+
+    # Create the controller and run it
     controller = PyCalcCtrl(model, view)
     controller.run()
+
+    # Execute calculator's main loop
     view.mainloop()
 
 
 def wxpython_app():
-    """Tkinter implementation."""
+    """wxPython implementation."""
     PyCalcConfig('config.ini').write()
+    # Import wx package
     import wx
 
+    # Import the view and the controller
     from pycalc.wx_view import PyCalcUi
     from pycalc.wx_controller import PyCalcCtrl
 
+    # Create an instance of wx.App
     pycalc = wx.App()
+
+    # Create the calculator's GUI
     view = PyCalcUi()
     pycalc.SetTopWindow(view)
+
+    # Create the model
     model = PyCalcModel()
+
+    # Create the controller and run it
     controller = PyCalcCtrl(model, view)
     controller.run()
+
+    # Execute calculator's main loop
     pycalc.MainLoop()
 
 
